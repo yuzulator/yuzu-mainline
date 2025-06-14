331e43f2de709528781e6acfaf40985e6acfe038
Mansoor Ahmed <mansooronly4u@gmail.com>
2025-06-14 00:10:41 -0400

    Updated the reference in scripts for docker image

--- .ci/scripts/linux/exec.sh
+++ .ci/scripts/linux/exec.sh
@@ -12,5 +12,5 @@ sudo chown -R 1027 ./
 # AZURECIREPO TITLEBARFORMATIDLE TITLEBARFORMATRUNNING DISPLAYVERSION
 #  are requested in src/common/CMakeLists.txt and appear to be provided somewhere in Azure DevOps
 
-docker run -e AZURECIREPO -e TITLEBARFORMATIDLE -e TITLEBARFORMATRUNNING -e DISPLAYVERSION -e ENABLE_COMPATIBILITY_REPORTING -e CCACHE_DIR=/yuzu/ccache -v "$(pwd):/yuzu" -w /yuzu yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.ci/scripts/linux/docker.sh "$1"
+docker run -e AZURECIREPO -e TITLEBARFORMATIDLE -e TITLEBARFORMATRUNNING -e DISPLAYVERSION -e ENABLE_COMPATIBILITY_REPORTING -e CCACHE_DIR=/yuzu/ccache -v "$(pwd):/yuzu" -w /yuzu yuzulator/build-environments:linux-fresh /bin/bash /yuzu/.ci/scripts/linux/docker.sh "$1"
 sudo chown -R $UID ./
